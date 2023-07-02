cdaff73b18edc8db5008c78f6fa5287f1f52ec57
Yusuke Doi <yuusukedoi23@gmail.com>
2023-07-03 00:24:01 +0900

    fix with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "plugins": ["prettier"],
-  "extends": ["react-app", "prettier"],
-  "rules": {
-    "prettier/prettier": "error"
-  }
+  plugins: ['prettier'],
+  extends: ['react-app', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
